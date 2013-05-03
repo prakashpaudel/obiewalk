b7d2302b8dcc0d2f08eb19db22aa3c7e1274a45d
Megan Davis <megan.davis@oberlin.edu>
2013-05-02 20:39:23 -0400

    Separated dijkstra from graph.py, removed non-functioning code.

---
+++ dijkstra.py
@@ -0,0 +1,11 @@
+# implementation of dijkstra's
+# obiewalk
+
+from graph import *
+
+# start : originating node
+# finish : end goal
+# graph : current graph
+def dijkstra(start, finish, graph):
+    # TODO: all
+    pass
